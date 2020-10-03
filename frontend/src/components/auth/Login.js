2726543f56b8afb7f9b197cf68b446cbdbda9fa2
Natalie Hurst-Knight <nhurst-knight@hotmail.com>
2020-10-03 23:03:32 +0100

    updated styling for login/register forms

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 
 import { loginUser } from '../../lib/api'
 import { setToken } from '../../lib/auth'
@@ -38,37 +39,55 @@ class Login extends React.Component {
   render() {
     const { email, password } = this.state.formData
     return (
-      <div className='loginForm'>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control 
-              type="email" 
-              placeholder="Enter email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-        
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control 
-              type="password" 
-              placeholder="Password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
-      </div>
+      <>
+        <Container fluid className="login-reg-banner">
+          <h1>Login</h1>
+        </Container>
+
+        <Container className="login-reg-wrapper">
+          <Row>
+            <Col className="login-reg-section">
+              <h3>Sign in</h3>
+              <div className='loginForm'>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control 
+                      type="email" 
+                      placeholder="Email"
+                      name="email"
+                      value={email}
+                      onChange={this.handleChange}
+                    />
+                    <Form.Text className="text-muted">
+                      {/* We'll never share your email with anyone else. */}
+                    </Form.Text>
+                  </Form.Group>
+                
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                      type="password" 
+                      placeholder="Password"
+                      name="password"
+                      value={password}
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" block>Login</Button>
+                </Form>
+              </div>
+            </Col>
+            
+            <Col className="login-reg-section">
+              <h3>New to The Beans?</h3>
+              <Link to="/register">
+                <Button variant="outline-primary" size="lg" >Create an account</Button>
+              </Link>
+            </Col>
+          </Row>
+        </Container> 
+      </>
     )
   }
 }
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
+
 import { registerUser } from '../../lib/api'
-import { Form, Button, Col } from 'react-bootstrap'
 
 class Register extends React.Component {
   state = {
@@ -41,101 +43,123 @@ class Register extends React.Component {
   }
 
   render() {
-    const { username, firstName, lastName, email, profileImage, password, passwordConfirmation } = this.state.formData
+    const { username, firstName, lastName, email, password, passwordConfirmation } = this.state.formData
     return (
-      <div className="registerForm">
-        <Form onSubmit={ this.handleSubmit }>
-          <Form.Group controlId="formBasicUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control 
-              className={`input ${this.state.errors.username ? 'is-danger' : ''}`}
-              type="username" 
-              name="username"
-              placeholder="Enter Username"
-              value={username}
-              onChange={this.handleChange}
-            />
-            <Form.Text className="text-muted">
-            </Form.Text>
-          </Form.Group>
+      <>
+        <Container fluid className="login-reg-banner">
+          <h1>Register</h1>
+        </Container>
 
-          <Form>
-            <Form.Row>
-              <Col> 
-                <Form.Control
-                  type="firstName" 
-                  placeholder="Enter First Name"
-                  name="firstName"
-                  value={firstName}
-                  onChange={this.handleChange}/>
-              </Col>
-              <Col>
-                <Form.Control 
-                  type="lastName" 
-                  placeholder="Enter Last Name"
-                  name="lastName"
-                  value={lastName}
-                  onChange={this.handleChange}/>
-              </Col>
-            </Form.Row>
-          </Form>
+        <Container className="login-reg-wrapper">
+          <Row>
+            <Col className="login-reg-section">
+              <h3>Sign in</h3>
+              <Link to="/login">
+                <Button variant="outline-primary" size="lg">I have an account</Button>
+              </Link>
+            </Col>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control 
-              type="Username" 
-              placeholder="Enter email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+            <Col className="login-reg-section">
+              <h3>New to The Beans?</h3>
+              <div className="registerForm">
+                <Form onSubmit={ this.handleSubmit }>
+                  <Form.Group controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                      className={`input ${this.state.errors.username ? 'is-danger' : ''}`}
+                      type="username" 
+                      name="username"
+                      placeholder="Username"
+                      value={username}
+                      onChange={this.handleChange}
+                    />
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                  </Form.Group>
 
-          <Form.Group controlId="formBasicUrl">
-            <Form.Label>Profile Picture</Form.Label>
-            <Form.Control 
-              className={`input ${this.state.errors.profileImage ? 'is-danger' : ''}`}
-              type="profileImage" 
-              name="profileImage"
-              placeholder="PLACEHOLDER URL LINK "
-              value={profileImage}
-              onChange={this.handleChange}
-            />
-            <Form.Text className="text-muted">
-            </Form.Text>
-          </Form.Group>
+                  <Form>
+                    <Form.Row>
+                      <Col> 
+                        <Form.Label>First name</Form.Label>
+                        <Form.Control
+                          type="firstName" 
+                          placeholder="First Name"
+                          name="firstName"
+                          value={firstName}
+                          onChange={this.handleChange}/>
+                      </Col>
+                      <Col>
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control 
+                          type="lastName" 
+                          placeholder="Last Name"
+                          name="lastName"
+                          value={lastName}
+                          onChange={this.handleChange}/>
+                      </Col>
+                    </Form.Row>
+                  </Form>
+
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control 
+                      type="Username" 
+                      placeholder="Email"
+                      name="email"
+                      value={email}
+                      onChange={this.handleChange}
+                    />
+                    <Form.Text className="text-muted">
+                      {/* We'll never share your email with anyone else. */}
+                    </Form.Text>
+                  </Form.Group>
+
+                  {/* <Form.Group controlId="formBasicUrl">
+                    <Form.Label>Profile Picture</Form.Label>
+                    <Form.Control 
+                      className={`input ${this.state.errors.profileImage ? 'is-danger' : ''}`}
+                      type="profileImage" 
+                      name="profileImage"
+                      placeholder="PLACEHOLDER URL LINK "
+                      value={profileImage}
+                      onChange={this.handleChange}
+                    />
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                  </Form.Group> */}
 
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control 
-              type="password" 
-              placeholder="Password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            <Form.Text className="text-muted">
-              Your password is protected in our database with bcrypt technology.
-            </Form.Text>
-          </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                      type="password" 
+                      placeholder="Password"
+                      name="password"
+                      value={password}
+                      onChange={this.handleChange}
+                    />
+                    <Form.Text className="text-muted">
+                      {/* Your password is protected in our database with bcrypt technology. */}
+                    </Form.Text>
+                  </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control 
-              type="password" 
-              placeholder="Password Confirmation"
-              name="passwordConfirmation"
-              value={passwordConfirmation}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit"> Register </Button>
-        </Form>
-      </div>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control 
+                      type="password" 
+                      placeholder="Password Confirmation"
+                      name="passwordConfirmation"
+                      value={passwordConfirmation}
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                  <Button className="switch-btn" variant="primary" type="submit" block> Register </Button>
+                </Form>
+              </div>
+            </Col>
+          </Row>
+        </Container> 
+      </>
     )
   }
 }
